d4e76f2c1ea7b41c8d0312e2079e821fd22adf85
Julián Melero Hidalgo <dev@julianmelero.dev>
2021-08-20 16:56:59 +0200

    Static Type exercices

---
+++ Typed/prime_number.py
@@ -0,0 +1,23 @@
+def is_prime(n: int) -> bool:
+    if n <= 1:
+        return False
+    elif n <= 3:
+        return True
+    elif n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+
+
+def run():
+    print(is_prime(55))
+
+
+
+if __name__ == "__main__":
+    run()
