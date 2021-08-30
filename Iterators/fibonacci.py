ba516ed42c9417951f38b6a6c6b1b205c01c5b84
Julián Melero Hidalgo <dev@julianmelero.dev>
2021-08-30 15:49:45 +0200

    Fibonacci Iterator

---
+++ Iterators/fibonacci.py
@@ -0,0 +1,25 @@
+
+
+class FiboIter():
+    """Iterator that yields numbers in the Fibonacci sequence."""
+
+    def __init__(self, max):
+        self.max = max
+
+    def __iter__(self):
+        self.a = 0
+        self.b = 1
+        return self
+
+    def __next__(self):
+        fib = self.a
+        if fib > self.max:
+            raise StopIteration
+        self.a, self.b = self.b, self.a + self.b
+        return fib
+
+
+
+fibo = FiboIter(10000)
+for i in fibo:
+    print(i)
