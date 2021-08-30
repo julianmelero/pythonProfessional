610df4fd9567d54dba1750b74a28cf9a62e81a0b
Julián Melero Hidalgo <dev@julianmelero.dev>
2021-08-30 16:58:08 +0200

    Sets: Union, Intersection, difference

--- Sets/set.py
+++ Sets/set.py
@@ -35,3 +35,18 @@ print(my_set)
 # .pop() --> delete random element
 # .clear() --> delete all elements
 
+
+# .union() --> return a new set with elements from both sets
+# .intersection() --> return a new set with elements common to both sets
+# .difference() --> return a new set with elements in the first set not in the second   
+my_set = {1, "Hello", (1, 2, 3), True}
+my_list = {4,5,6,7,8}
+my_set2 = my_set.union(my_list)
+my_set3 = my_set | my_list
+my_set4 = my_set & my_list
+my_set5 = my_set - my_list
+my_set6 = my_set ^ my_list
+print(my_set3)
+print(my_set4)
+print(my_set5)
+print(my_set6)
